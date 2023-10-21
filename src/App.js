b9fdd4de2c4d96aebf44176593db22de7de4e2da
Cocreatorpattern <136176709+Cocreatorpattern@users.noreply.github.com>
2023-10-21 02:50:01 +0100

    created frame

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Weather from "./Weather";
+ 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <h1>
+          Love Elshaddai</h1>
+          <Weather city="Lagos"/>  
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
